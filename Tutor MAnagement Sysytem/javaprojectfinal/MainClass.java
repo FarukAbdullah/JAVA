5ef559d1623af3892230c101c9667fc7bbb44874
Faruk Abdullah <50788914+FarukAbdullah@users.noreply.github.com>
2021-03-26 01:10:09 +0600

    Add files via upload

--- Tutor MAnagement Sysytem/javaprojectfinal/MainClass.java
+++ Tutor MAnagement Sysytem/javaprojectfinal/MainClass.java
@@ -1,1 +1,11 @@
+package javaprojectfinal;
 
+public class MainClass {
+
+    public static void main(String[] args) {
+       
+        Welcome w1 = new Welcome();
+        w1.setVisible(true);
+        
+    }
+}
